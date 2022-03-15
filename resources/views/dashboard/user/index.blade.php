f3fd23fe0cb0aa8527d8e4498d193d0f4a745fe9
rehman101 <73483363+rehman101@users.noreply.github.com>
2022-03-15 17:44:41 +0500

    push first time

---
+++ resources/views/dashboard/user/index.blade.php
@@ -0,0 +1,27 @@
+@extends('dashboard.layout.main')
+
+@section('content')
+
+
+
+
+    <!-- Dashboard Start -->
+    <section class="page-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 p-0">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- Dashboard End -->
+
+
+@endsection
